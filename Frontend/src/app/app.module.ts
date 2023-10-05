a8c268e7438f73d492be54f4618a8d2abf6d66a4
Gary Xiloj <grxdc10@gmail.com>
2023-10-04 21:06:56 -0600

    Creacion de vista de inicio de sesión

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+/** Material Designe */
+import { MatInputModule } from '@angular/material/input';
+
+/** Componentess */
+import { LoginComponent } from './Components/login/login.component';
+import { ToastComponent } from './Components/toast/toast.component';
+
+/** Servicios */
+import { ToastService } from './Services/toast.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		ToastComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule,
+		NgbModule,
+		HttpClientModule,
+  		BrowserAnimationsModule,
+		MatInputModule
+	],
+	providers: [
+		ToastService
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
