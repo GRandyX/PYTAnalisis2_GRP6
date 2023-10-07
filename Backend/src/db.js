3ab2eecd3d5f13fbbc83909aa379c10db05b01d8
Gary Xiloj <grxdc10@gmail.com>
2023-10-06 23:02:43 -0600

    Cambio de data DataBase

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -3,14 +3,14 @@ import {createPool} from 'mysql2/promise'
 //Cambiar las configuraciones según las bd
 export const pool = createPool({
     host: 'localhost',
-    user: 'root', 
-    password: 'admin',
+    user: 'adminpytpast',
+    password: 'adminpyt',
     port: 3306,
-    database: 'analisis_past'   
+    database: 'analisis_past'
 });
 
 
 
 
 
- 
+
