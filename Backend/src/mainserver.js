599e03277be8ccd0780d000599b3d1be61287d83
Gary Xiloj <grxdc10@gmail.com>
2023-10-06 21:39:33 -0600

    Instalacion de CORS y cambio de puerto

--- Backend/src/mainserver.js
+++ Backend/src/mainserver.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
 import usuariosRoutes from './routes/usuarios.routes.js'
+import cors from 'cors'
 
 const app = express()
 
-const port = process.env.port || 80
+const port = process.env.port || 8000
 app.set('json spaces', 2)
 
 app.use(morgan('dev'))
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
@@ -15,4 +17,3 @@ app.use('/api',usuariosRoutes)
 
 app.listen(port, () => console.log(`Escuchando en puerto ${port}...`))
 
- 
